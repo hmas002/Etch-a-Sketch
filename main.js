c592fbc3dbefcefc1b7d22ff179fbda2ae5d14d9
hamza <hmas002@protonmail.com>
2022-10-13 14:37:28 -0400

    Add option to color square on hover

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
+function sketchSquare() {
+    const squares = document.querySelectorAll('.square');
+    console.log(squares);
+    squares.forEach(square => square.addEventListener('mouseover', () => {
+        square.classList.add('sketched-square');
+    }));
+}
+
+
 // create a grid using givens rows and columsn params
-function createGrid(rows, columns) {
+function createGrid(gridSize) {
     const container = document.querySelector('.container');
     console.log(container);
 
     //for each row create a square div for that row
-    for(let i = 0; i < rows; i++) {
+    for(let i = 0; i < gridSize; i++) {
         const row = document.createElement('div');
         //console.log(row);
         row.classList.add('row');
-        for(let j = 0; j < columns; j++) {
-            const column = document.createElement('div');
-            column.classList.add('column');
-            row.appendChild(column);
+        for(let j = 0; j < gridSize; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            row.appendChild(square);
         }
         container.appendChild(row);
-
     }
+    sketchSquare();
 }
